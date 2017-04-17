2e08cc3dde4732b3175b15023539adde1afe0b4a
Chris McKenzie <crmckenzie@gmail.com>
2017-04-17 16:17:06 -0700

    initial commit

---
+++ src/HelloWorld.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class extends Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (
+      <div className="hello-world">
+        <h1 />
+        <div>Hello, World!</div>
+      </div>
+      );
+  }
+}
